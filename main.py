dfa534b1faa5c2856a4e0fc499e0221e3aad4f91
BIng1980 <bxu7@buffalo.edu>
2023-08-26 11:03:50 -0400

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def main():
-    print('Hello World!')
+    book, author, release_year, goodreads_rating = 'Dracula', 'Bram Stoker', 1897, 4.01
+
+    # print(book + ' is a novel by ' + author + ', published in ' + str(release_year) + '. It has a rating of ' + str(goodreads_rating) + ' on goodreads.')
+
+# f string format is better
+    print(f'{book} is a novel by {author}, published in {release_year}.'
+          f' It has a rating of {goodreads_rating} on goodreads.')
 
 # program entry point
 if __name__ == '__main__':
