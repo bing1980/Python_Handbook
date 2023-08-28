72988ac09b0d17512fa4770366bad3b1a303abe6
BIng1980 <bxu7@buffalo.edu>
2023-08-27 22:26:47 -0400

    Initial commit

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def main():
 #           f' It has a rating of {goodreads_rating} on goodreads.')
 
 #-------------------------------------------
+# work with input()
+# -------------------------------------------
 
-# # work with input()
 #     name = input('What is your name? ')
 #     age = int(input(f'How old are you {name}? '))
 #     current_year = int(input(f'What year is this again? '))
@@ -18,8 +19,8 @@ def main():
 #     print(f'If my calculations are right, you were born in {current_year - age}')
 
 #-------------------------------------------
-
 # work with strings
+#-------------------------------------------
 # triple quotes - You can put multi-line strings within triple quotes and Python will preserve the white spaces as well.
 #     synopsis = """Dracula comprises journal entries, letters, and telegrams written by the main characters.
 #         It begins with Jonathan Harker, a young English lawyer, as he travels to Transylvania.
@@ -48,13 +49,85 @@ def main():
     # print(a_random_list)
 
 # pop() method get rid of the last value in a list
-    horror_books = ['Dracula', 'Carmilla', 'The Imago Sequence']
-
-    print(horror_books.pop())
-    print(horror_books)
+#     horror_books = ['Dracula', 'Carmilla', 'The Imago Sequence']
+#
+#     print(horror_books.pop())
+#     print(horror_books)
 
 #-------------------------------------------
-# Tuples in Python
+# Tuples in Python (allow mixed types)
+# -------------------------------------------
+
+#     a_random_list = ('Dracula', 1, 5.7, 'Carmilla')
+#
+#     print(a_random_list)
+
+#-------------------------------------------
+# Ranges in Python
+# -------------------------------------------
+
+# a_range = range(5, 15)  # 5 到 14
+    #
+    # print(a_range)
+    #
+    # list_a_range = list(a_range)
+    #
+    # print(list_a_range)
+    #
+    # tuple_a_range = tuple(a_range)
+    #
+    # print(tuple_a_range)
+
+
+#--------------------------------------------
+# for loop
+#--------------------------------------------
+
+    # random_numbers = [6, 1, 3, 8, 0, 9, 12, 3, 4, 0, 54, 8, 100, 55, 60, 70, 85]
+    # multiplied_random_numbers = []
+    #
+    # for number in random_numbers:
+    #     multiplied_random_numbers.append(number * 2)
+    #
+    # print(multiplied_random_numbers)
+
+#--------------------------------------------
+# while loop
+#--------------------------------------------
+
+    # number = 1
+    # while number < 11:
+    #     print(number)
+    #     number += 1
+
+# --------------------------------------------
+# Use the + and * Operators
+# --------------------------------------------
+# The + operator lets you merge two sequences together.
+#     books = ['Dracula', 'Frankenstein', 'The Omen', 'The Exorcist', 'The Legend of Sleepy Hollow']
+#     more_books = ['And Then There Were None', 'The ABC Murders', 'The Valley of Fear', 'The Hound of the Baskervilles',
+#                   'The Chestnut Man']
+#
+#     print(books + more_books)
+
+# The * operator, on the other hand, makes multiple copies of a given sequence.
+    books = ['Dracula', 'Frankenstein', 'The Omen', 'The Exorcist', 'The Legend of Sleepy Hollow']
+
+    print(books * 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
